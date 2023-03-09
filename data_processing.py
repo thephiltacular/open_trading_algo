1a0870d44305cc14576dcb66d252979c2da2288b
thephiltacular <philipemai@gmail.com>
2023-03-08 20:57:17 -0500

    added lamdas for cols SO through UK

--- data_processing.py
+++ data_processing.py
@@ -32,6 +32,7 @@ class Model():
                     "alerts": {}}
         # data = pd.DataFrame()
 
+        print("Reading in all data and alerts in:", data_folder)
         with open("raw_data.txt") as files:
             for file in files:
                 file = file.replace("\n", "")
@@ -39,109 +40,211 @@ class Model():
                 if "A" in file:  # Alert file
                     name = file.replace("A.csv", "")
                     self.data_dict["alerts"][name] = df_temp
-                    print(name)
+                    # print(name)
                 else:  # Data file
                     name = file.replace("D.csv", "")
                     self.data_dict["data"][name] = df_temp
-                    print(name)
+                    # print(name)
 
         self.data = self.data_dict["data"]
         self.calculate_relative_volume_div_by()
         self.calculate_d_relative_volume_up_or_down()
+        self.calculate_ADX_filtered_RSI()
+        self.calculate_volatility()
         ###################################### End __init__
+
+    def calculate_ADX_filtered_RSI(self):
+        print("Calculating ADX Filtered RSI columns...")
+        cols_out = [
+                "ADX Filtered RSI (7)",
+                "ADX Filtered RSI (14)"
+            ]
+        cols_val = [
+                "Average Directional Index (14)",
+                "Average Directional Index (14)"
+            ]
+        cols_left = [
+                "Relative Strength Index (7)",
+                "Relative Strength Index (14)",
+            ]
+        cols_right = [
+                "Stochastic RSI Fast (3, 3, 14, 14)",
+                "Stochastic RSI Slow (3, 3, 14, 14)",
+            ]
+        cols_to_print = ["Ticker"]
+        cols_to_print = cols_to_print + cols_out + cols_val + cols_left + cols_right
+        for day, df in self.data.items():
+            for col_out, val, left, right in zip(cols_out, cols_val, cols_left, cols_right):
+                df[col_out] = df.apply(
+                            lambda row: self.ADX_filtered_RSI_lambda(row[val], row[left], row[right], row["Description"]),
+                            axis=1)
+            print(df[cols_to_print])
+        print("Done!")
+        
+    
+    def ADX_filtered_RSI_lambda(self, val, left, right, description):
+        if description is not None:
+            if val > 20:
+                return left
+            else:
+                return right
+        else:
+            return 0
+    
+    def calculate_volatility(self):
+        print("Calculating volatility columns...")
+        cols_out = [
+                "Volatility D/W",
+                "Volatility D/M",
+                "Volatility W/M",
+                "Bollinger Price/Upper-1",
+                "Bollinger Price/Lower-1",	
+                "Bollinger Upper/Lower Band (20)",
+                "Keltner CH Price/Upper-1",
+                "Keltner CH Price/Lower-1",	
+                "Keltner Channels Upper/Lower Band (20)",
+                "Donchian CH Price/Lower-1",
+                "Donchian CH Price/Upper-1"
+                "Relative Strength Index (7/14)",
+                "ADX Filtered RSI (7/14)"
+            ]
+        print(cols_out)
+        numerator_cols = [
+                "Volatility",
+                "Volatility Week",
+                "Volatility Month",
+                "Price",
+                "Price",
+                "Bollinger Upper Band (20)",
+                "Price",
+                "Price",
+                "Keltner Channels Upper Band (20)",
+                "Price",
+                "Price",
+                "Relative Strength Index(7)",
+                "ADX Filtered RSI (7)",
+            ]
+        denominator_cols = [
+                "Volatility Week",
+                "Volatility Month",
+                "Volatility Month",
+                "Bollinger Upper Band (20)",
+                "Bollinger Lower Band (20)",
+                "Bollinger Lower Band (20)",
+                "Keltner Channels Upper Band (20)",
+                "Keltner Channels Lower Band (20)",
+                "Keltner Channels Lower Band (20)",
+                "Donchian Channels Lower Band (20)",
+                "Donchian Channels Upper Band (20)",
+                "Relative Strength Index(14)",
+                # "ADX Filtered RSI (14)",
+            ]
+        cols_to_print = ["Ticker"]
+        cols_to_print = cols_to_print + cols_out + numerator_cols + denominator_cols
+        for day, df in self.data.items():
+            for col_out, num_col, den_col in zip(cols_out, numerator_cols, denominator_cols):
+                df[col_out] = df.apply(
+                            lambda row: self.volatility_lambda(row[num_col], row[den_col], row["Description"]),
+                            axis=1)
+            # print(df[cols_to_print])
+        print("Done!")
+
+    def volatility_lambda(self, numerator, denominator, description):
+        if description is not None and denominator != 0:
+            return numerator/denominator - 1
         
     def calculate_relative_volume_div_by(self):
-            
-            cols_out = ["/10",
-                    "/30",
-                    "/60",
-                    "/90",
-                    " 10/30"]
-            cols = ["Average Volume (10 day)",
-                    "Average Volume (30 day)",
-                    "Average Volume (60 day)",
-                    "Average Volume (90 day)"]
-            for day, df in self.data.items():
-                # print("=======================")
-                # print(day)
-                cols_to_print = ["Ticker"]
-                # Calculate d-relative volume col
-                # df["D-Relative Volume"] = df.apply(
-                #         lambda row: self.d_relative_volume(row["Description"], row["Relative Volume"]),
-                #         axis=1
-                #     )
-                # Calculate 
-                for col_out, col in zip(cols_out, cols):
-                    col_out_name = "Relative Volume" + col_out
-                    df[col_out_name] = df.apply(
-                        lambda row: self.relative_volume_div(row["Volume"], row[col], row["Description"]),
-                        axis=1)
-                    # percentile_rank_col = col_out_name + "(pct rank)"
-                    # print("col:", col, "col_out:", col_out_name, "pct_rank:", percentile_rank_col)
-                    # df[percentile_rank_col] = df[col_out_name].rank(pct=True)
-                    cols_to_print += [col, 
-                                      col_out_name, 
-                                    #   percentile_rank_col
-                                      ]
-                    # print(df[percentile_rank_col].head())
-                
-                # print(cols_to_print)
-                # print(df[cols_to_print])
-                
-
-    def calculate_d_relative_volume_up_or_down(self):
-            
-            cols_out = [
-                "D-Relative Volume-Up",
-                "Relative Volume/10-Up",
-                "Relative Volume/30-Up",
-                "Relative Volume/60-Up",
-                "Relative Volume/90-Up",
-                "D-Relative Volume-Down",
-                "Relative Volume/10-Down",
-                "Relative Volume/30-Down",
-                "Relative Volume/60-Down",
-                "Relative Volume/90-Down"
-            ]
-            cols = [
-                "D-Relative Volume",
-                "Average Volume (10 day)",
-                "Average Volume (30 day)",
-                "Average Volume (60 day)",
-                "Average Volume (90 day)",
-                "D-Relative Volume",   
-                "Average Volume (10 day)",
+        print("Calculating relative volume div columns...")
+        cols_out = ["/10",
+                "/30",
+                "/60",
+                "/90",
+                " 10/30"]
+        cols = ["Average Volume (10 day)",
                 "Average Volume (30 day)",
                 "Average Volume (60 day)",
                 "Average Volume (90 day)"]
-            for day, df in self.data.items():
-                print("=======================")
-                print(day)
-                cols_to_print = ["Ticker"]
-                # Calculate d-relative volume col
-                df["D-Relative Volume"] = df.apply(
-                        lambda row: self.d_relative_volume(row["Relative Volume"], row["Description"]),
-                        axis=1
-                    )
-                # Calculate 
-                for col_out, col in zip(cols_out, cols):
-                    # col_out_name = "Relative Volume" + col_out
-                    # up = True 
-                    if "Up" in col_out:
-                        # up = False
-                        df[col_out] = df.apply(
-                            lambda row: self.d_relative_volume_up(row[col], row["Description"]),
-                            axis=1)
-                    else:
-                        df[col_out] = df.apply(
-                            lambda row: self.d_relative_volume_down(row[col], row["Description"]),
-                            axis=1)
-                    cols_to_print += [col, col_out]
-                    # print(df[percentile_rank_col].head())
-                
-                print(cols_to_print)
-                print(df[cols_to_print])
+        for day, df in self.data.items():
+            # print("=======================")
+            # print(day)
+            cols_to_print = ["Ticker"]
+            # Calculate d-relative volume col
+            # df["D-Relative Volume"] = df.apply(
+            #         lambda row: self.d_relative_volume(row["Description"], row["Relative Volume"]),
+            #         axis=1
+            #     )
+            # Calculate 
+            for col_out, col in zip(cols_out, cols):
+                col_out_name = "Relative Volume" + col_out
+                df[col_out_name] = df.apply(
+                    lambda row: self.relative_volume_div(row["Volume"], row[col], row["Description"]),
+                    axis=1)
+                # percentile_rank_col = col_out_name + "(pct rank)"
+                # print("col:", col, "col_out:", col_out_name, "pct_rank:", percentile_rank_col)
+                # df[percentile_rank_col] = df[col_out_name].rank(pct=True)
+                cols_to_print += [col, 
+                                    col_out_name, 
+                                #   percentile_rank_col
+                                    ]
+                # print(df[percentile_rank_col].head())
+            
+            # print(cols_to_print)
+            # print(df[cols_to_print])
+        print("Done!")
 
+    def calculate_d_relative_volume_up_or_down(self):
+        print("Calculating d-relative volume up and down cols")
+        cols_out = [
+            "D-Relative Volume-Up",
+            "Relative Volume/10-Up",
+            "Relative Volume/30-Up",
+            "Relative Volume/60-Up",
+            "Relative Volume/90-Up",
+            "D-Relative Volume-Down",
+            "Relative Volume/10-Down",
+            "Relative Volume/30-Down",
+            "Relative Volume/60-Down",
+            "Relative Volume/90-Down"
+        ]
+        cols = [
+            "D-Relative Volume",
+            "Average Volume (10 day)",
+            "Average Volume (30 day)",
+            "Average Volume (60 day)",
+            "Average Volume (90 day)",
+            "D-Relative Volume",   
+            "Average Volume (10 day)",
+            "Average Volume (30 day)",
+            "Average Volume (60 day)",
+            "Average Volume (90 day)"]
+        for day, df in self.data.items():
+            # print("=======================")
+            # print(day)
+            cols_to_print = ["Ticker"]
+            # Calculate d-relative volume col
+            df["D-Relative Volume"] = df.apply(
+                    lambda row: self.d_relative_volume(row["Relative Volume"], row["Description"]),
+                    axis=1
+                )
+            # Calculate 
+            for col_out, col in zip(cols_out, cols):
+                # col_out_name = "Relative Volume" + col_out
+                # up = True 
+                if "Up" in col_out:
+                    # up = False
+                    df[col_out] = df.apply(
+                        lambda row: self.d_relative_volume_up(row[col], row["Description"]),
+                        axis=1)
+                else:
+                    df[col_out] = df.apply(
+                        lambda row: self.d_relative_volume_down(row[col], row["Description"]),
+                        axis=1)
+                cols_to_print += [col, col_out]
+                # print(df[percentile_rank_col].head())
+            
+            # print(cols_to_print)
+            # print(df[cols_to_print])
+        print("Done!")
        
     def d_relative_volume(self, relative_volume, description):
         return relative_volume - 1.0 if description is not None and relative_volume != 0 else 0
